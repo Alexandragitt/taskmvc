345f5c1950bb35969cd25582e24591c916744cb5
Riabova Alexandra <you@example.com>
2018-08-16 23:35:33 +0300

    Dya

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,32 +3,30 @@ require_once ('/models/Site.php');
 require_once ('/models/UploadForm.php');
 class SiteController
 {
-    const COUNTELEMENT=3;
+    const COUNTELEMENT = 3;
 
 
     public function actionIndex($page = 1)
     {
-        $numberPage = ($page - 1) * self::COUNTELEMENT;
-        $page = (int)$numberPage;
+        $numberPage = (int) ($page - 1) * self::COUNTELEMENT;
+        $page = $numberPage;
         $newCountTasks = Site::getCountTasks();
         $countPage = $newCountTasks[0] / self::COUNTELEMENT;
         $countPage = ceil($countPage);
-        $uploadForm = new UploadForm();
-
         $arrayTasks = Site::getArrayTasks($page);
         require_once('/../views/Site/index.php');
         if (!empty($_POST)) {
-            $newTask= array();
+            $newTask = array();
             foreach ($_POST as $key => $value) {
                 $value = htmlspecialchars(strip_tags(trim($value)));
                 $newTask[$key] = $value;
             }
             if (!empty($_FILES)) {
                 $uploadsDir = ROOT . '\uploads';
-                $fileName= $uploadForm->hash($_FILES["file"]["name"]);
-                $targetFile= $uploadForm->getNewFileTarget($fileName, $uploadsDir);
+                $fileName = UploadForm::hash($_FILES["file"]["name"]);
+                $targetFile =UploadForm::getNewFileTarget($fileName, $uploadsDir);
                 if ($_FILES['file']['error'] == 0) {
-                    if ($uploadForm->uploadFile($_FILES['file']['tmp_name'], $targetFile)) {
+                    if (UploadForm::uploadFile($_FILES['file']['tmp_name'], $targetFile)) {
                         Site::insertTask($newTask, $fileName);
                         echo 'Создана задача';
                     } else {
--- models/UploadForm.php
+++ models/UploadForm.php
@@ -1,16 +1,16 @@
 <?php
 class UploadForm{
 
-    public function hash($name){
-        $fileName = md5($_FILES["file"]["name"] . Uniqid());
+    public static function hash($name){
+        $fileName = md5($name . Uniqid());
         return $fileName;
 
     }
-    public function getNewFileTarget($file, $dir){
+    public static function getNewFileTarget($file, $dir){
         $targetFile = $dir . '\\' . $file . '.jpeg';
         return $targetFile;
     }
-    public function uploadFile($file, $newFile){
+    public static function uploadFile($file, $newFile){
         move_uploaded_file($file, $newFile);
         return true;
 
